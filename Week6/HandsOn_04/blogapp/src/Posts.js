15ea418f97037947841cf3676cad8c56db8586ae
Abhishek Kumar Anand <abhishek9117834305@gmail.com>
2025-07-25 14:09:39 +0530

    Added week 06 Assignment

---
+++ Week6/HandsOn_04/blogapp/src/Posts.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import Post from './Post';
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      error: null
+    };
+  }
+
+  loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not OK');
+        }
+        return response.json();
+      })
+      .then(data => {
+        const postList = data.map(
+          post => new Post(post.id, post.title, post.body)
+        );
+        this.setState({ posts: postList });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    alert("An error occurred: " + error.toString());
+    console.error("Error Info: ", info);
+  }
+
+  render() {
+    const { posts, error } = this.state;
+
+    if (error) {
+      return <h2>Error loading posts</h2>;
+    }
+
+    return (
+      <div>
+        <h1>Blog Posts</h1>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
